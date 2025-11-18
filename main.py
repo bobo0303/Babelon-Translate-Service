884597a8e30b5c533b04d4c046cead21a05d7b4a
bobo0303 <wiwiwiwi6166@gmail.com>
2025-11-18 14:59:55 +0000

    [feat] We remove the prev_text in 0.5 sec audio. (multi_strategy_transcription=1)

--- main.py
+++ main.py
@@ -286,6 +286,11 @@ async def translate(
         A response containing the transcription results.  
     """  
     
+    # 20251118 we found if we use prev_text in 0.5 sec audio it will cause worse results
+    if multi_strategy_transcription == 1:
+        prev_text = ""  # Clear previous text if only one strategy is used
+    
+    
     # Convert times to string format  
     times = str(times)  
     # Convert original language and target language to lowercase  
